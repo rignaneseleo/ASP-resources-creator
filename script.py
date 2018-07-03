ccfa44eb716ec92969ce3ad111d33b8be860f268
Leo <dev.rignaneseleo@gmail.com>
2018-07-03 15:20:09 +0200

    Bugfix

--- script.py
+++ script.py
@@ -35,8 +35,8 @@ def add_content_to_file(path, file_content, content_to_add):
 resources_content = {}
 # resources where to look for resource keys
 resources = [
-    r"C:\Users\user\Documents\projects\X\src\WebSite\App_GlobalResources\Resource1.resx",
-    r"C:\Users\user\Documents\projects\X\src\WebSite\App_GlobalResources\Resource2.resx",
+    r"C:\Users\lrignanese\Documents\Progetti\Aeroporto\branches\4_2_0\src\WebSite\App_GlobalResources\AeroportoResources.resx",
+    r"C:\Users\lrignanese\Documents\Progetti\Aeroporto\branches\4_2_0\src\WebSite\App_GlobalResources\Validation.resx"
 ]
 # languages of other resources where to add the resources
 languages = [
@@ -55,14 +55,15 @@ while 1:
     resource_key = get_camel_case_string(string_to_replace)
 
     print()
-    print("Generated resource key: '{0}' → {1}".format(string_to_replace, resource_key))
+    print("Generated resource key: '{0}' → {1}".format(
+        string_to_replace, resource_key))
 
     # check if the resource key is present in any of the resources files
-    for resource_name, resource_content in resources_content.items():
+    for resource_path, resource_content in resources_content.items():
         if '"' + resource_key + '"' in resource_content:
             # it exists
             # save the resource file path
-            resource_name = get_file_name_from_path(resource_name)
+            resource_name = get_file_name_from_path(resource_path)
             print()
             print("Resource already present in " + resource_name)
 
@@ -87,22 +88,25 @@ while 1:
 
             # add the resource key in other resources
             for language in languages:
-                other_resource = resources[i_resource_chosen].replace(".resx", ".{0}.resx".format(language))
+                other_resource = resources[i_resource_chosen].replace(
+                    ".resx", ".{0}.resx".format(language))
                 content_to_add = """\r\n<data name="{0}" xml:space="preserve"><value>#{1}#</value><comment>TODO: Translate</comment></data>""".format(
                     resource_key, escape(string_to_replace))
                 content = read_content_from_path(other_resource)
                 add_content_to_file(other_resource, content, content_to_add)
-                print("Resource correctly added in the {0} resource file.".format(language))
+                print(
+                    "Resource correctly added in the {0} resource file.".format(language))
 
             # save the resource file path
-            resource_name = get_file_name_from_path(resources[i_resource_chosen])
+            resource_name = get_file_name_from_path(
+                resources[i_resource_chosen])
         else:
             print("Error")
     print()
 
     if resource_name is not "":
         # here I wrote some way to get and use a resource
-        print("You can get this resource using one of this codes:")
+        print("You can get this resource using one of this snippets:")
         print()
         print("""\t<%$ Resources:{0}, {1} %>""".format(
             resource_name, resource_key))
